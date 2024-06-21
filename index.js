f6e5c901db9dcf051b8a687a7b526cb12c2cd15b
Naima <naima.lalmi28@gmail.com>
2024-06-21 16:06:12 +0200

    marqueur coworking ok

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+//MAP INTEGRATION//
+//var map = L.map("map").setView([46.227638,2.213749],13);
+var map_Paris = L.map("map").setView([48.866667, 2.333333], 13);
+
+//Ajout des tuiles à la map
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map_Paris);
+
+//Ajout Marqueur de Paris 
+var long = 2.35
+var lat = 48.866667;
+var marker = L.marker([lat, long]).addTo(map_Paris);
+ marker.bindPopup("PARIS") 
+ //.openPopup(); 
+
 const ulElement = document.querySelector("ul");
+
 fetch(
   "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/coworking-france/records?limit=40&refine=ville%3A%22Paris%22"
   // "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/coworking-france/records?limit=20&refine=ville%3A%22Paris%22"
@@ -8,25 +26,35 @@ fetch(
     console.log(data);
     const element = data.results;
     for (let i = 0; i < element.length; i++) {
+    
       const liElement = document.createElement("li");
       const title = document.createElement("h2");
       title.innerText = element[i].nom;
       const adress = document.createElement("span");
       adress.innerText = element[i].adresse;
+      const cp = document.createElement('span');
+      cp.innerText = element[i].cp;
+      const ville = document.createElement("span");
+      ville.innerText = ` ${element[i].ville}`;
+      const lien = document.createElement("a");
+      lien.href = lien.innerHTML = element[i].web;
+      //
+        //Marqueur Coworking + donnée lat/long
+        const longtitude = element[i].coordonnees.lon;
+        const latitude = element[i].coordonnees.lat;
+        var marker1 = L.marker([latitude,longtitude]).addTo(map_Paris)
+        marker1.bindPopup(`${element[i].nom}`)
+
+      //
+      ulElement.appendChild(liElement);
       liElement.appendChild(title);
       liElement.appendChild(adress);
-      ulElement.appendChild(liElement);
+      liElement.appendChild(document.createElement("br")); // Ajoute un saut de ligne
+      liElement.appendChild(cp);
+      liElement.appendChild(ville);
+      liElement.appendChild(document.createElement("br")); // Ajoute un saut de ligne
+      liElement.appendChild(lien)
+    
     }
   });
 
-//MAP INTEGRATION//
-//Coordonnée de PARIS //
-var map = L.map("map").setView([48.866667, 2.333333], 13);
-//Ajout des tuiles à la map
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-//Ajout Marqueur de Paris 
-var marker = L.marker([48.866667, 2.333333]).addTo(map);
-marker.bindPopup("PARIS ").openPopup();
